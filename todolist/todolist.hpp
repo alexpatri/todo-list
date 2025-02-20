816b8e471ed8a81939a7681d0474d7b39789afe3
Alexsandro <jalexsandro2005@gmail.com>
2025-02-20 10:40:53 -0300

    feat: created Task class delcaration

--- todolist/todolist.hpp
+++ todolist/todolist.hpp
@@ -2,9 +2,28 @@
 
 #define TODOLIST_HPP
 
+#include <ctime>
+#include <string>
+
 namespace todo {
-// TODO:
-// - Criar as classes: Task, Section e List
-}
+// TODO: Criar a class Task
+class Task {
+public:
+  Task(const std::string &, int);
+  ~Task();
+
+  bool switch_status(void);
+
+private:
+  std::string decription;
+  std::time_t date;
+  bool is_finished;
+  int section;
+};
+
+// TODO: Criar a class Section
+//
+// TODO: Criar a class List
+} // namespace todo
 
 #endif // !TODOLIST_HPP
